5321e8fc2de047ddce542df81d24bff141cab031
Arthur <bss.rol20@gmail.com>
2022-07-14 02:32:46 +0900

    input multi

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,23 +1,40 @@
 import React, {useState} from 'react';
 
 function InputSample() {
-    const [text, setText] = useState('');
+    // const [text, setText] = useState('');a
+    const[inputs, setInputs] = useState({
+      name :"",
+      nick : ""
+    });
+
+    const {name, nick} = inputs; //비구조화
     
     const onChange = (e) => {
-      setText(e.target.value);
+      // setText(e.target.value);
+      const {value, name} = e.target;
+      setInputs ({
+        ...inputs,
+        [name] : value
+      });      
     };
   
     const onReset = () => {
-      setText('');
+      // setText(''); 
+      setInputs({
+        name : "",
+        nick : "",
+      })
     };
   
     return (
       <div>
-        <input onChange={onChange} value={text} />
+      <input  name= "name" placeholder="name" onChange={onChange} value={name}/>
+        <input  name= "nick" placeholder="nick" onChange={onChange} value={nick}/>
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
-      </div>
+        <b>값: </b>
+        {name} ({nick})
+              </div>
     </div>
   );
 }
